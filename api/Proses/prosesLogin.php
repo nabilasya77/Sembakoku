a76db271b84636c8b5e2fd54e326423226e07465
nabilasya77 <nabilacheisya_7@student.uns.ac.id>
2026-05-23 10:06:22 +0700

    UP

--- api/Proses/prosesLogin.php
+++ api/Proses/prosesLogin.php
@@ -1,6 +1,6 @@
 <?php
 
-include __DIR__ . '/../server/koneksi.php';
+include __DIR__ . '/../Server/koneksi.php';
 
 $username = mysqli_real_escape_string($koneksi, $_POST['username']);
 $password = $_POST['password'];
